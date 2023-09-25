44d9596a796203b49d2010ca3427169c8d87aba4
Nabeel Rana <nabwaregroup@gmail.com>
2023-09-25 15:54:22 -0700

    Completed up to 6

--- 03_last_element.py
+++ 03_last_element.py
@@ -12,3 +12,4 @@ def last_element(lst):
         >>> nums == [1, 2, 3]
         True
     """
+    return lst[-1] if lst else None
--- 06_single_letter_count.py
+++ 06_single_letter_count.py
@@ -1,12 +1,20 @@
 def single_letter_count(word, letter):
     """How many times does letter appear in word (case-insensitively)?
-    
+
         >>> single_letter_count('Hello World', 'h')
         1
-        
+
         >>> single_letter_count('Hello World', 'z')
         0
-        
+
         >>> single_letter_count("Hello World", 'l')
         3
     """
+    # letter_sum = 0
+
+    # for char in word:
+    #     if char.lower() == letter.lower(): letter_sum += 1
+
+    # return letter_sum
+
+    return sum([1 for c in word if c.lower() == letter.lower()])
